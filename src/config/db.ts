b4b1b93b29f7bfef39aac6e6d449e0591d50dc46
Ivandro Neto <ivandroneto1@outlook.com>
2025-04-01 14:15:42 +0100

    initialize project with TypeScript, Express, and Sequelize setup

---
+++ src/config/db.ts
@@ -0,0 +1,32 @@
+import { Sequelize } from "sequelize-typescript";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const sequelize = new Sequelize({
+  dialect: "mssql",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  logging: false,
+  dialectOptions: {
+    options: {
+      encrypt: true, // Habilitar SSL se necessário
+      trustServerCertificate: true, // Para conexões locais
+    },
+  },
+});
+
+export const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("✅ Database connected successfully!");
+  } catch (error) {
+    console.error("❌ Database connection failed:", error);
+    process.exit(1);
+  }
+};
+
+sequelize.addModels([Customers]);
